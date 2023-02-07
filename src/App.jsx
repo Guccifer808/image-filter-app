ca51e08b271917690132294720a962ab878a5af6
G808 <hucciallstars@gmail.com>
2023-02-07 21:32:49 +0200

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container, Box } from '@mui/material';
+
+function App() {
+  return (
+    <Container sx={{ marginTop: '4rem', marginBottom: '4rem' }}>
+      <Box></Box>
+    </Container>
+  );
+}
+
+export default App;
