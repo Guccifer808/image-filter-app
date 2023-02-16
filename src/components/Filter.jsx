a22eac88f07a1f9883a85e545897210d0bb59d4c
G808 <hucciallstars@gmail.com>
2023-02-16 23:21:15 +0200

    Fixed error for build

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { Box } from '@mui/system';
-import { filterValues } from '../utils/filterValues';
+import { filterValues } from '../utils/FilterValues';
 import { FilterContext } from './../App';
 
 const Filter = () => {
