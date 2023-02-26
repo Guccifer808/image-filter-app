dade661847fe396f692850448fbab666e240e114
G808 <hucciallstars@gmail.com>
2023-02-26 16:35:07 +0200

    Responsive design edits

--- src/components/FiltersSection.jsx
+++ src/components/FiltersSection.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
-import { Box, Tabs, Tab } from '@mui/material';
+import { Box, Tabs, Tab, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 import { FilterContext } from '../App';
 
@@ -16,13 +17,22 @@ const FiltersSection = () => {
       setFilterClass('');
     }
   };
+  //Imported useMediaQuery and useTheme from @mui/material and @mui/material/styles respectively.
+  // Used useMediaQuery to get the current screen size and stored it in isMobile variable.
+  // Used useTheme to access the theme object and stored it in theme variable.
+  // Added a conditional statement to adjust the marginBottom of the Box component based on the screen size.
+  // Added variant prop to the Tabs component and set it to fullWidth for mobile devices and standard for larger screens.
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Box sx={{ marginBottom: '2rem' }}>
+    <Box sx={{ marginBottom: isMobile ? '1rem' : '2rem' }}>
       <Tabs
         textColor='primary'
         indicatorColor='primary'
         value={sectionFilter}
         onChange={handleChange}
+        variant={isMobile ? 'fullWidth' : 'standard'}
       >
         <Tab value='filter' label='Filter' />
         <Tab value='advancedFilter' label='Advanced Filter' />
